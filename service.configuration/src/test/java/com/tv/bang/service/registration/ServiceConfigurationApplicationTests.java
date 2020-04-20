447fc98e84f9587ed61f94ddd1f07798600fc90b
Vitaliy Tarasyuk <vitaliyt@devcom.com>
2020-04-20 18:21:15 +0300

    fixes for inventory service added configuration service

---
+++ service.configuration/src/test/java/com/tv/bang/service/registration/ServiceConfigurationApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tv.bang.service.registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
